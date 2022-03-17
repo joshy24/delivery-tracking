e1ee01b1ed0a6e38edf054da7be3bed01fe9ac0b
joshy24 <joshuamajebi@yahoo.com>
2022-03-17 21:07:40 +0100

    setup general modules

---
+++ config/config.js
@@ -0,0 +1,17 @@
+'use strict'
+
+module.exports = {
+    name: 'Delivery ',
+    version: '1.0.0',
+    env: process.env.NODE_ENV,
+    port: process.env.PORT,
+    //base_url: process.env.BASE_URL,
+    db: {
+        uri: process.env.DB_URL,
+        options: {
+            useNewUrlParser: true, 
+            useCreateIndex: true,
+            useUnifiedTopology: true
+        }
+    },
+}
--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 'use strict'
 
-require ("dotenv").config()
+//require ("dotenv").config()
 
 const express = require("express")
 
+const log = require('./modules/logger.module')
+const mongoose = require('./modules/mongoose.module')
+
 const app = express()
 
-const port = process.env.PORT || 3000
+app.use(express.json({ type: 'application/json', limit: '8mb' }));
+app.use(express.urlencoded({limit: '8mb', extended: false}));
+
+app.disable('x-powered-by');
+
+const port = process.env.PORT || 4000
 
 app.listen(port, () => {
+    log.info(`Server is running on port ${port}`)
     
+    /*mongoose.connect(config.db.uri, config.db.options)
+        .then(()=> { log.info(`Succesfully Connected to the Mongodb Database  at URL : `+config.db.uri)})
+        .catch((error)=> { log.error(error)})*/
 })
 
 
