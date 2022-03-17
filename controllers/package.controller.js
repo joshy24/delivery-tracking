31e261b665135469b321dd072f1d350ec5f82849
joshy24 <joshuamajebi@yahoo.com>
2022-03-17 22:22:20 +0100

    setup controller

--- controllers/package.controller.js
+++ controllers/package.controller.js
@@ -1,1 +1,23 @@
 'use strict'
+
+const packageService = require("../services/package.service");
+
+module.exports.createPackage = async (req,res) => {
+
+}
+
+module.exports.getAllPackages = async (req,res) => {
+
+}
+
+module.exports.getPackage = async (req,res) => {
+
+}
+
+module.exports.updatePackage = async (req,res) => {
+
+}
+
+module.exports.deletePackage = async (req,res) => {
+
+}
--- routes/package.routes.js
+++ routes/package.routes.js
@@ -1,26 +1,25 @@
 'use strict'
 
-const packageController = require("../controllers/package.controller")
-
+const { createPackage, getAllPackages, getPackage, updatePackage, deletePackage } = require("../controllers/package.controller")
 
 module.exports = (router) => {
+    router.post("/create_package", (req,res) => {
+        createPackage(req,res)
+    })
+
     router.get("/get_all_packages", (req,res) => {
-        
+        getAllPackages(req,res)
     })
 
     router.get("/get_package/:id", (req,res) => {
-        
-    })
-
-    router.post("/create_package", (req,res) => {
-        
+        getPackage(req,res)
     })
 
     router.put("/update_package", (req,res) => {
-        
+        updatePackage(req,res)
     })
 
     router.delete("/delete_package/:id", (req,res) => {
-        
+        deletePackage(req,res)
     })
 }
